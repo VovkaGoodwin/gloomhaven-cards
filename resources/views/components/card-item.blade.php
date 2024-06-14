b64b0498fc4f0951b02e603aa8c343ad692d8fee
Vovka Goodwin <volodya.radko@gmail.com>
2024-06-14 11:00:46 +0700

    Добавил карточки

--- resources/views/components/card-item.blade.php
+++ resources/views/components/card-item.blade.php
@@ -5,23 +5,26 @@
     }
   </script>
 @endpushonce
-<div style="flex: 0 0 30%">
+<div style="flex: 1 0 30%">
   <div class="card shadow">
     <div class="card-header d-flex justify-content-between align-items-center">
       <div>{{ $card->title }}</div>
+
+    </div>
+    <div class="card-body" style="min-height: 200px">
+      {{ $card->text }}
+    </div>
+    <div class="card-body d-flex justify-content-between align-items-center">
       <div>
         <form action="/cards/{{ $card->id }}" method="post" onsubmit="return onDeleteCardHandler('{{ $card->title }}')">
-          <button class="btn btn-outline-danger">Удалить</button>
+          <button class="btn btn-outline-danger btn-sm">Удалить</button>
           @csrf
           @method('DELETE')
         </form>
       </div>
       <div>
-        <button type="button" class="btn btn-link" onclick="window.location = `/cards/{{ $card->id }}/edit`">Редактировать</button>
+        <button type="button" class="btn btn-outline-primary btn-sm" onclick="window.location = `/cards/{{ $card->id }}/edit`">Редактировать</button>
       </div>
-    </div>
-    <div class="card-body" style="min-height: 200px">
-      {{ $card->text }}
     </div>
     <div class="card-footer">
       #{{ $card->number }}
